c99de1323a7e12abaaece12c1ed290e3b5b02b33
Michel Vollebregt <mi.vollebregt@gmail.com>
2020-01-19 13:56:58 +0100

    Fetching user ids for participating users

--- src/app/shared/event.service.ts
+++ src/app/shared/event.service.ts
@@ -18,41 +18,43 @@ export class EventService {
   ) {
   }
 
-  getUserEvents(): Observable<Event[]> {
-    return this.withUserId(userId => this.getEventsByUser(userId));
-  }
-
   getFollowingEvents(): Observable<Event[]> {
     return this.withUserId(userId =>
-      this.getFollowingByUser(userId).pipe(
-        map(followings => [userId, ...followings.map(following => following.targetId)]),
-        switchMap(userIds => this.getEventsByUser(...userIds))));
-  }
-
-  private getEventsByUser(...userIds: string[]): Observable<Event[]> {
-    return this.getParticipationsByUser(...userIds).pipe(
-      map(participations => [...new Set(participations.map(participation => participation.eventId))]),
-      switchMap(eventIds => this.getEventsById(...eventIds)));
-  }
-
-  private getParticipationsByUser(...userIds: string[]): Observable<Participation[]> {
-    return this.db.collection<Participation>('participations', ref =>
-      ref.where('userId', 'in', userIds)).valueChanges();
-  }
-
-  private getEventsById(...ids: string[]): Observable<Event[]> {
-    return zip(
-      ...ids.map(id => this.db.doc<Event>(`events/${id}`).valueChanges())
-    );
-  }
-
-  private getFollowingByUser(userId: string): Observable<Following[]> {
-    return this.db.collection<Following>(`users/${userId}/following`).valueChanges();
+      this.getFollowedUserIdsForUserId(userId).pipe(
+        switchMap(userIds => this.getParticipationsForUserIds(userIds)),
+        switchMap(participations => this.getEventsForParticipations(participations))));
   }
 
   private withUserId<T>(func: (userId: string) => Observable<T[]>): Observable<T[]> {
     return this.afAuth.authState.pipe(
       switchMap(user => user ? func(user.uid) : [])
     );
+  }
+
+  private getFollowedUserIdsForUserId(userId: string): Observable<string[]> {
+    return this.db.collection<Following>(`users/${userId}/following`).valueChanges().pipe(
+      map(followings => [userId, ...followings.map(following => following.targetId)])
+    );
+  }
+
+  private getParticipationsForUserIds(userIds: string[]): Observable<Participation[]> {
+    return this.db.collection<Participation>('participations', ref =>
+      ref.where('userId', 'in', userIds)).valueChanges();
+  }
+
+  private getEventsForParticipations(participations: Participation[]): Observable<Event[]> {
+    return zip(...this.partitionedByEvent(participations).map(eventIdWithParticipations => this.getEvent(eventIdWithParticipations)));
+  }
+
+  private getEvent([eventId, participatingUserIds]: [string, string[]]): Observable<Event> {
+    return this.db.doc<Event>(`events/${eventId}`).valueChanges().pipe(map(event => ({...event, participatingUserIds})));
+  }
+
+  private partitionedByEvent(participations: Participation[]): [string, string[]][] {
+    const result = new Map<string, string[]>();
+    for (const participation of participations) {
+      result.set(participation.eventId, [...(result.get(participation.eventId) || []), participation.userId]);
+    }
+    return [...result.entries()];
   }
 }
